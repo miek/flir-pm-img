14da9e1ab8941a33326d9dbe09fe3cde2b47142f
Mike Walters <mike@flomp.net>
2019-03-12 13:44:29 +0000

    Use Pillow to output an image

--- extract.py
+++ extract.py
@@ -1,11 +1,43 @@
 import flir
+import numpy as np
 import struct
 import sys
 from kaitaistruct import KaitaiStream
+from PIL import Image, ImageFile, ImageMath
 
-def clamp(x):
-    return min(max(0, int(x)), 65535)
+def reject_outliers(data, m = 2.):
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d/(mdev if mdev else 1.)
+    return data[s<m]
 
-x = flir.Flir(KaitaiStream(open(sys.argv[1], 'rb')))
-data = list(map(lambda x: clamp((x-32000)*60), x.imagedata.data))
-sys.stdout.buffer.write(struct.pack('H'*len(data), *data))
+class FlirPMImageFile(ImageFile.ImageFile):
+    format = "FlirPM"
+    format_description = "FLIR PM-series camera IMG format"
+
+    def _open(self):
+        f = flir.Flir(KaitaiStream(self.fp))
+        self.f = f
+
+        self.size = f.metadata.width, f.metadata.height
+        self.mode = "F"
+
+        self.tile = [
+            ("raw", (0, 0) + self.size, 0x438, ('F;16B', 0, 1)),
+        ]
+
+        data = reject_outliers(np.array(f.imagedata.data), 10.)
+
+        self.min = min(data)
+        self.max = max(data)
+
+if len(sys.argv) < 3:
+    print("Usage: {} <input.img> <output>".format(sys.argv[0]))
+    sys.exit()
+
+Image.register_open("FlirPM", FlirPMImageFile)
+Image.register_extension("FlirPM", ".IMG")
+
+im = Image.open(sys.argv[1])
+im = ImageMath.eval("convert((a-{})*{}, 'L')".format(im.min, 256.0/(im.max-im.min)), a=im)
+im.save(sys.argv[2])
